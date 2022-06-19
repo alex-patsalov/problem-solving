4fc4a920b2dedc4e1845fe44e762c6f1f5365e22
Alex Patsalov <alexpatsalov@gmail.com>
2022-06-19 18:27:05 +0300

    Remove duplicates problem

---
+++ src/main/java/easy/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package easy;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+    public int removeDuplicates(int[] nums) {
+        int pointer = 1;
+        int prevElement = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] != prevElement){
+                nums[pointer] = nums[i];
+                prevElement = nums[i];
+                pointer++;
+            }
+        }
+        return pointer;
+    }
+
+}
